ffd4920eb79d452d3296ccb2eb72cd1bbdce57d9
Habib Yahya <nmh.habib.y@gmail.com>
2021-03-02 16:03:26 +0700

    little fix

--- Assets/Scripts/Habib/HybridRope.cs
+++ Assets/Scripts/Habib/HybridRope.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class HybridRope : MonoBehaviour
 {
     [SerializeField] private GameObject linkPrefab = null;
+    [SerializeField] private GameObject endPrefab = null;
     [SerializeField] private int numberOfLinks = 5;
     [SerializeField] private Rigidbody2D player = null;
     [SerializeField] private float ropeLen = 0.3f;
     [SerializeField] private Vector2 anchorVal = new Vector2();
     [SerializeField] private Vector2 connAnchorVal = new Vector2();
     [SerializeField] private float ropeWidth = 0.1f;
+    [SerializeField] private float colliderRad = 0.5f;
     private LineRenderer lineRenderer;
     List<GameObject> generatedLinks = new List<GameObject>();
 
@@ -34,9 +36,16 @@ public class HybridRope : MonoBehaviour
         for (int i = 0; i < numberOfLinks; i++)
         {
             GameObject theObject;
-            theObject = Instantiate(linkPrefab, transform.position - new Vector3(0, i * ropeLen, 0), transform.rotation);
+            if(i == numberOfLinks - 1)
+                theObject = Instantiate(endPrefab, transform.position - new Vector3(0, i * ropeLen, 0), transform.rotation);
+            else
+                theObject = Instantiate(linkPrefab, transform.position - new Vector3(0, i * ropeLen, 0), transform.rotation);
             SpringJoint2D joint = theObject.GetComponent<SpringJoint2D>();
+            CircleCollider2D circleCollider = theObject.GetComponent<CircleCollider2D>();
             joint.connectedBody = prevRB;
+
+            if(circleCollider)
+                circleCollider.radius = colliderRad;
 
             joint.anchor = anchorVal;
             joint.connectedAnchor = connAnchorVal;
