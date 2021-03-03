a4397c2e210cf2e4b48e21772bf06e9afa2e0ef9
babylion <btarialiya93@gmail.com>
2021-03-03 18:06:46 +0700

    popUp

---
+++ Assets/Scripts/PopUpManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PopUpManager : MonoBehaviour
+{
+    Scene curScene;
+
+    void Start(){
+        curScene = SceneManager.GetActiveScene();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(curScene.name);
+        Time.timeScale = 1;
+    }
+
+    public void Menu()
+    {
+        Debug.Log("Menu");
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    public void NextLevel()
+    {
+        Debug.Log("Next Level");
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
