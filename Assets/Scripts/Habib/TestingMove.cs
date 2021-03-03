77c44c5c364616df4f74d82261b324a98ea1a4c6
Habib Yahya <nmh.habib.y@gmail.com>
2021-03-03 11:41:26 +0700

    fixing changes

--- Assets/Scripts/Habib/TestingMove.cs
+++ Assets/Scripts/Habib/TestingMove.cs
@@ -17,7 +17,6 @@ public class TestingMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         if (Input.GetKey(KeyCode.A))
         {
             rb2d.AddForce(new Vector2(-speed * Time.deltaTime, 0f), ForceMode2D.Impulse);
@@ -34,33 +33,5 @@ public class TestingMove : MonoBehaviour
         {
             rb2d.AddForce(new Vector2(0, speed * Time.deltaTime), ForceMode2D.Impulse);
         }
-=======
-        // if (Input.GetKey(KeyCode.A))
-        // {
-        //     rb2d.AddForce(new Vector2(-speed, 0f));
-        // }
-        // else if (Input.GetKey(KeyCode.D))
-        // {
-        //     rb2d.AddForce(new Vector2(speed, 0f));
-        // }
-        // else if (Input.GetKey(KeyCode.S))
-        // {
-        //     rb2d.AddForce(new Vector2(0, -speed));
-        // }
-        // else if (Input.GetKey(KeyCode.W))
-        // {
-        //     rb2d.AddForce(new Vector2(0, speed));
-        // }
-
-        // ver 1
-        movement.x = Input.GetAxis("Horizontal") * speed;
-        movement.y = Input.GetAxis("Vertical") * speed;
-
-        rb2d.AddForce(new Vector2(movement.x, movement.y));
-
-        // ver 2
-        // Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        // rb2d.AddForce(move * speed);
->>>>>>> 0383e6c30f1139bfdc0528ba3cd00c2fac0a9ea3
     }
 }
